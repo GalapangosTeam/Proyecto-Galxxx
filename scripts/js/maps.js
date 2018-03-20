c8c5d6589439c030752b3d97e3f9f7b13152e436
Unknown <visualyukkie@hotmail.es>
2018-03-20 02:06:29 -0600

    Avanze
    Modal

--- scripts/js/maps.js
+++ scripts/js/maps.js
@@ -11,54 +11,6 @@ function initMap() {
 }
 
 /*VENTANA MODAL*/
-<<<<<<< HEAD:scripts/maps.js
-/*
-$('.formulario').find('input, textarea').on('keyup blur focus', function (e) {
-  
-  var $this = $(this),
-      label = $this.prev('label');
-
-	  if (e.type === 'keyup') {
-			if ($this.val() === '') {
-          label.removeClass('active highlight');
-        } else {
-          label.addClass('active highlight');
-        }
-    } else if (e.type === 'blur') {
-    	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
-			} else {
-		    label.removeClass('highlight');   
-			}   
-    } else if (e.type === 'focus') {
-      
-      if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
-      else if( $this.val() !== '' ) {
-		    label.addClass('highlight');
-			}
-    }
-
-});
-
-$('.tab a').on('click', function (e) {
-  
-  e.preventDefault();
-  
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-  
-  target = $(this).attr('href');
-
-  $('.tab-content > div').not(target).hide();
-  
-  $(target).fadeIn(600);
-  
-}); */
-
-=======
->>>>>>> mockupsminePeter:scripts/js/maps.js
 document.getElementsByClassName("enlaceOpcion")[0].click();
 
 function opcModal(evt, opcion) {
@@ -78,8 +30,4 @@ function opcModal(evt, opcion) {
 
 $('#datepicker').datepicker({
             uiLibrary: 'bootstrap4'
-<<<<<<< HEAD:scripts/maps.js
-        });
-=======
 });
->>>>>>> mockupsminePeter:scripts/js/maps.js
